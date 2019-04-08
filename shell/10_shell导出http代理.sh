cd935e7726690906a80adfd9ecbf4c4c6af4cfd9
zh <zhouhui@winext.cn>
2019-04-08 11:36:37 +0800

    .

--- shell/10_shell导出http代理.sh
+++ shell/10_shell导出http代理.sh
@@ -2,7 +2,7 @@
 
 pxySrv=127.0.0.1:8118
 
-function shpxy(){
+function shpxy (){
 	case "$1" in 
 	   "on") export http_proxy=$pxySrv https_proxy=$pxySrv ;;
 	   "off") unset http_proxy https_proxy ;;
